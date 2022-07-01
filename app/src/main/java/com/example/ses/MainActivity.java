12c5ea8bcec988f895b584cd5ee839e165eef67c
sans <valerasulgina@gmail.com>
2022-07-01 23:44:26 +0700

    1

--- app/src/main/java/com/example/ses/MainActivity.java
+++ app/src/main/java/com/example/ses/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginPassword";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
 
         EditText text1 = (EditText) findViewById(R.id.log);
         EditText text2 = (EditText) findViewById(R.id.pas);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(text1.getText().toString().equals("") || text2.getText().toString().equals("")){
+                    Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Intent j = new Intent(MainActivity.this,MainActivity2.class);
+                    startActivity(j);
+                }
+            }
+        });
 
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/ses/MainActivity2.java
+++ app/src/main/java/com/example/ses/MainActivity2.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -15,6 +17,23 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         Button button1 = (Button) findViewById(R.id.zad);
+        Button button2 = (Button) findViewById(R.id.reg);
+
+        EditText text1 = (EditText) findViewById(R.id.txt1);
+        EditText text2 = (EditText) findViewById(R.id.txt2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(text1.getText().toString().equals("") || text2.getText().toString().equals("")){
+                    Toast.makeText(MainActivity2.this,"не верный логин или пароль",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Intent i = new Intent(MainActivity2.this,MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
